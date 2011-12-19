f7a36e006d5932a7b3f3e1478fc577bf7bb303d9
Mateusz Loskot <mateusz@loskot.net>
2011-12-19 00:47:11 +0000

    Updated message at the end of init process to reflect Python port specific options.

--- lib/gitsvnmirror.py
+++ lib/gitsvnmirror.py
@@ -122,7 +122,7 @@ class GitSVNMirror(object):
         self.log("The mirror workbench has been configured. To push to the remote Git repo,",
             "and possibly as a cron job, run the following command:",
             "",
-            "  git-svn-mirror update '%s'" % self.workbench)
+            "  git-svn-mirror update -w '%s'" % self.workbench)
 
     def update(self):
         self.fetch()
